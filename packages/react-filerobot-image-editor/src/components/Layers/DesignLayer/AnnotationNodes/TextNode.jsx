f68076b9fbbe668fac4246a557d9490bb1b751f0
Alex <akhachoyan698@gmail.com>
2024-07-15 17:30:48 +0400

    feat: New Features

--- packages/react-filerobot-image-editor/src/components/Layers/DesignLayer/AnnotationNodes/TextNode.jsx
+++ packages/react-filerobot-image-editor/src/components/Layers/DesignLayer/AnnotationNodes/TextNode.jsx
@@ -5,7 +5,7 @@ import { Text } from 'react-konva';
 
 /** Internal Dependencies */
 import nodesCommonPropTypes from '../nodesCommonPropTypes';
-import RectNode from './RectNode';
+// import RectNode from './RectNode';
 
 const TextNode = ({
   id,
@@ -37,66 +37,66 @@ const TextNode = ({
   rectConfig,
   ...otherProps
 }) => (
-  <>
-    {rectConfig && (
-      <RectNode
-        id={id}
-        name={name}
-        scaleX={scaleX}
-        scaleY={scaleY}
-        stroke={stroke}
-        strokeWidth={strokeWidth}
-        shadowOffsetX={shadowOffsetX}
-        shadowOffsetY={shadowOffsetY}
-        shadowBlur={shadowBlur}
-        shadowColor={shadowColor}
-        shadowOpacity={shadowOpacity}
-        fontFamily={fontFamily}
-        fontStyle={fontStyle}
-        fontSize={fontSize}
-        letterSpacing={letterSpacing}
-        lineHeight={lineHeight}
-        align={align}
-        x={x}
-        y={y}
-        width={width}
-        height={height}
-        annotationEvents={annotationEvents}
-        {...rectConfig}
-      />
-    )}
-    <Text
-      id={id}
-      name={name}
-      rotation={rotation}
-      scaleX={scaleX}
-      scaleY={scaleY}
-      stroke={stroke}
-      strokeWidth={strokeWidth}
-      shadowOffsetX={shadowOffsetX}
-      shadowOffsetY={shadowOffsetY}
-      shadowBlur={shadowBlur}
-      shadowColor={shadowColor}
-      shadowOpacity={shadowOpacity}
-      opacity={opacity}
-      fill={fill}
-      text={text}
-      fontFamily={fontFamily}
-      fontStyle={fontStyle}
-      fontSize={fontSize}
-      letterSpacing={letterSpacing}
-      lineHeight={lineHeight}
-      align={align}
-      x={x}
-      y={y}
-      width={width}
-      height={height}
-      {...annotationEvents}
-      {...otherProps}
-    />
-  </>
+  // <>
+  //   {rectConfig && (
+  //     <RectNode
+  //       {...rectConfig}
+  //       text={text}
+  //       id={id}
+  //       name={name}
+  //       scaleX={scaleX}
+  //       scaleY={scaleY}
+  //       stroke={stroke}
+  //       strokeWidth={strokeWidth}
+  //       shadowOffsetX={shadowOffsetX}
+  //       shadowOffsetY={shadowOffsetY}
+  //       shadowBlur={shadowBlur}
+  //       shadowColor={shadowColor}
+  //       shadowOpacity={shadowOpacity}
+  //       fontFamily={fontFamily}
+  //       fontStyle={fontStyle}
+  //       fontSize={fontSize}
+  //       letterSpacing={letterSpacing}
+  //       lineHeight={lineHeight}
+  //       align={align}
+  //       x={x}
+  //       y={y}
+  //       width={width}
+  //       height={height}
+  //       annotationEvents={annotationEvents}
+  //     />
+  //   )}
+  <Text
+    id={id}
+    name={name}
+    rotation={rotation}
+    scaleX={scaleX}
+    scaleY={scaleY}
+    stroke={stroke}
+    strokeWidth={strokeWidth}
+    shadowOffsetX={shadowOffsetX}
+    shadowOffsetY={shadowOffsetY}
+    shadowBlur={shadowBlur}
+    shadowColor={shadowColor}
+    shadowOpacity={shadowOpacity}
+    opacity={opacity}
+    fill={fill}
+    text={text}
+    fontFamily={fontFamily}
+    fontStyle={fontStyle}
+    fontSize={fontSize}
+    letterSpacing={letterSpacing}
+    lineHeight={lineHeight}
+    align={align}
+    x={x}
+    y={y}
+    width={width}
+    height={height}
+    {...annotationEvents}
+    {...otherProps}
+  />
+  // </>
 );
-
 TextNode.defaultProps = {
   ...nodesCommonPropTypes.defaults,
   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur imperdiet tortor quis odio facilisis, id aliquet nulla facilisis. Etiam tincidunt tempor odio nec placerat.',
@@ -125,16 +125,23 @@ TextNode.propTypes = {
   lineHeight: PropTypes.number,
   align: PropTypes.string,
   rectFill: PropTypes.string,
-  rectConfig: PropTypes.shape({
-    ...nodesCommonPropTypes.definitions,
-    x: PropTypes.number.isRequired,
-    y: PropTypes.number.isRequired,
-    annotationEvents: PropTypes.instanceOf(Object).isRequired,
-    width: PropTypes.number,
-    height: PropTypes.number,
-    fill: PropTypes.string,
-    cornerRadius: PropTypes.number,
-  }),
+  // rectConfig: PropTypes.shape({
+  //   rotation: PropTypes.number,
+  //   scaleX: PropTypes.number,
+  //   scaleY: PropTypes.number,
+  //   stroke: PropTypes.string,
+  //   strokeWidth: PropTypes.number,
+  //   shadowOffsetX: PropTypes.number,
+  //   shadowOffsetY: PropTypes.number,
+  //   shadowBlur: PropTypes.number,
+  //   shadowColor: PropTypes.string,
+  //   shadowOpacity: PropTypes.number,
+  //   opacity: PropTypes.number,
+  //   width: PropTypes.number,
+  //   height: PropTypes.number,
+  //   fill: PropTypes.string,
+  //   cornerRadius: PropTypes.number,
+  // }),
 };
 
 export default TextNode;
